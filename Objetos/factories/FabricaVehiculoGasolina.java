cf7ae00e9f3e7173bb402ae3c10c1466195f358a
aledeleonez <98777405+aledeleonez@users.noreply.github.com>
2022-03-22 18:27:54 +0100

    Objetos
    Construir los objetos de dominio (coche de gasolina, coche diesel, coche eléctrico, etc.).

---
+++ Objetos/factories/FabricaVehiculoGasolina.java
@@ -0,0 +1,22 @@
+package factories;
+
+import automovil.Automovil;
+import automovil.AutomovilGasolina;
+import scooter.Scooter;
+import scooter.ScooterGasolina;
+
+public class FabricaVehiculoGasolina implements FabricaVehiculo  
+{  
+  public Automovil creaAutomovil(String modelo, String  
+    color, int potencia, double espacio)  
+  {  
+    return new AutomovilGasolina(modelo, color,  
+      potencia, espacio);  
+  }  
+  
+  public Scooter creaScooter(String modelo, String  
+    color, int potencia)  
+  {  
+    return new ScooterGasolina(modelo, color, potencia);  
+  }  
+}
