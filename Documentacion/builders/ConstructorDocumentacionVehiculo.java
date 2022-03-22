98ce3c91d81e211fc3a4fdab1bca762b29315fbe
aledeleonez <98777405+aledeleonez@users.noreply.github.com>
2022-03-22 18:30:16 +0100

    Documentación
    Construir los conjuntos de documentos necesarios en caso de comprar un vehículo.

---
+++ Documentacion/builders/ConstructorDocumentacionVehiculo.java
@@ -0,0 +1,19 @@
+package builders;
+
+import documentacion.Documentacion;
+
+public abstract class ConstructorDocumentacionVehiculo  
+{  
+    protected Documentacion documentacion;  
+  
+    public abstract void construyeSolicitudPedido(String  
+      nombreCliente);  
+  
+    public abstract void construyeSolicitudMatriculacion  
+      (String nombreSolicitante);  
+  
+    public Documentacion resultado()  
+    {  
+        return documentacion;  
+    }  
+} 
---
+++ Documentacion/builders/ConstructorDocumentacionVehiculoPdf.java
@@ -0,0 +1,31 @@
+package builders;
+
+import documentacion.DocumentacionPdf;
+
+public class ConstructorDocumentacionVehiculoPdf extends  
+  ConstructorDocumentacionVehiculo  
+{  
+    public ConstructorDocumentacionVehiculoPdf()  
+    {  
+        documentacion = new DocumentacionPdf();  
+    }  
+  
+    public void construyeSolicitudPedido(String  
+      nombreCliente)  
+    {  
+        String documento;  
+        documento = "<PDF>Solicitud de pedido Cliente: " +  
+          nombreCliente + "</PDF>";  
+        documentacion.agregaDocumento(documento);  
+    }  
+  
+    public void construyeSolicitudMatriculacion  
+      (String nombreSolicitante)  
+    {  
+        String documento;  
+        documento =  
+        "<PDF>Solicitud de matriculacion Solicitante: " +  
+        nombreSolicitante + "</PDF>";  
+        documentacion.agregaDocumento(documento);  
+    }  
+}
