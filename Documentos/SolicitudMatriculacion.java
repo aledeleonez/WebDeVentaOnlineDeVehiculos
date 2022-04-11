64b055112d63a06061f0a47cfb458d2072e7e77f
aledeleonez <98777405+aledeleonez@users.noreply.github.com>
2022-04-11 10:39:26 +0100

    Patrón Prototype

---
+++ Documentos/SolicitudMatriculacion.java
@@ -0,0 +1,16 @@
+import documentacion.Documento;
+
+public class SolicitudMatriculacion extends Documento  
+{  
+  public void visualiza()  
+  {  
+    System.out.println(  
+      "Muestra la solicitud de matriculacion: " + contenido);  
+  }  
+  
+  public void imprime()  
+  {  
+    System.out.println(  
+      "Imprime la solicitud de matriculacion: " + contenido);  
+  }  
+} 
